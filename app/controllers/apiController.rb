5901749fb7179143c83a4e625e127d1cdba5a7bd
gbs4ever2@gmail.com <gbs4ever2@gmail.com>
2020-05-27 22:42:43 -0400

    webhook verifaction data working

--- app/controllers/apiController.rb
+++ app/controllers/apiController.rb
@@ -1,6 +1,27 @@
 class ApiController < ApplicationController
-
+skip_before_action :verify_authenticity_token
+ SHARED_SECRET = '037f2a4d0da5fdc52386eb9aa2c5d1ef3384d2a2dab503033cded56e0ddcf3ef'
     def welcome
         render  :json => {status: "loaded shopify app dev"}
     end
+    def return
+       # verification for hook finished 
+        request.body.rewind
+        data = request.body.read
+        if data != ""
+        
+        verified = verify_webhook(data,request.env["HTTP_X_SHOPIFY_HMAC_SHA256"])
+        else 
+            binding.pry
+        end
+        puts "Webhook verified: #{verified}"
+        
+
+    end
+      private
+      def verify_webhook(data, hmac_header)
+      calculated_hmac = Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', SHARED_SECRET, data))
+       ActiveSupport::SecurityUtils.secure_compare(calculated_hmac, hmac_header)
+    
+  end
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
    get "/home", to: 'api#welcome'
+    post  "/return", to: 'api#return'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
